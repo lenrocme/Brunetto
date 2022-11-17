78084818ff4278d3fe9f3accd0a418fa841465bb
Corneliu <mcornelius@mail.ru>
2022-11-17 19:32:44 +0100

    add data view model for screen taxes

--- app/src/main/java/com/example/brunetto/viewModels/CalcTaxViewModel.kt
+++ app/src/main/java/com/example/brunetto/viewModels/CalcTaxViewModel.kt
@@ -2,12 +2,16 @@ package com.example.brunetto.viewModels
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class CalcTaxViewModel : ViewModel() {
-    val rentenVers : Double by mutableStateOf(0.0)
-    val arbeitslosenVers : Double by mutableStateOf(0.0)
-    val krankVers : Double by mutableStateOf(0.0)
-    val kvZusatzBetr : Double by mutableStateOf(0.0)
-    val plfegVers : Double by mutableStateOf(0.0)
+
+    // Social versicherung part of the tax
+    var totalSocialVers : Double by mutableStateOf(0.0)
+    var rentenVers : Double by mutableStateOf(0.0)
+    var arbeitslosenVers : Double by mutableStateOf(0.0)
+    var krankVers : Double by mutableStateOf(0.0)
+    var kvZusatzBetr : Double by mutableStateOf(0.0)
+    var plfegVers : Double by mutableStateOf(0.0)
 }
