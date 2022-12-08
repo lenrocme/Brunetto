3cb026405b3043e744244ae0ec5085801d7a7671
Corneliu <mcornelius@mail.ru>
2022-12-08 17:39:09 +0100

    add default data in for init entity

--- app/src/main/java/com/example/brunetto/data/lastInput/LastInput.kt
+++ app/src/main/java/com/example/brunetto/data/lastInput/LastInput.kt
@@ -12,79 +12,79 @@ data class LastInput (
     val id: Int,
 
     @ColumnInfo(name = "salaryBrut")
-    val salaryBrut: Double,
+    val salaryBrut: Double = 40000.0,
     @ColumnInfo(name = "timePeriod")
-    val timePeriod: Double,
+    val timePeriod: Double = 1.0,
     @ColumnInfo(name = "isProYear")
-    val isProYear: Boolean,
+    val isProYear: Boolean = true,
 
     /**
      * Dropdown menus
      * */
     @ColumnInfo(name = "birthday")
-    val birthday: Double,
+    val birthday: Double = 0.0,
     @ColumnInfo(name = "taxClass")
-    val taxClass: Double,
+    val taxClass: Double = 1.0,
     @ColumnInfo(name = "onClassFour")
-    val onClassFour: Double,
+    val onClassFour: Double = 0.0,
     @ColumnInfo(name = "childrenConst")
-    val childrenConst: Double,
+    val childrenConst: Double = 0.0,
     @ColumnInfo(name = "statNumb")
-    val statNumb: Double,
+    val statNumb: Int = 1,
     @ColumnInfo(name = "churchTax")
-    val churchTax: Double,
+    val churchTax: Double = 8.0,
 
     /**
      * Checkboxes
      * */
     @ColumnInfo(name = "noChildren")
-    val noChildren: Boolean,
+    val noChildren: Boolean = true,
     @ColumnInfo(name = "pensionIns")
-    val pensionIns: Boolean,
+    val pensionIns: Boolean = true,
     @ColumnInfo(name = "noJobIns")
-    val noJobIns: Boolean,
+    val noJobIns: Boolean = true,
 
     /**
      * Health insurance
      * */
     @ColumnInfo(name = "healthIns")
-    val healthIns: Double,
+    val healthIns: Double = 14.6,
     @ColumnInfo(name = "additionalAmount")  // Zusatzbetrag
-    val additionalAmount: Double,
+    val additionalAmount: Double = 1.3,
 
     /**
      * Private insurance
      * */
     @ColumnInfo(name = "isPrivateIns")
-    val isPrivateIns: Boolean,
+    val isPrivateIns: Boolean = false,
     @ColumnInfo(name = "anpkvPayedPremium")
-    val anpkvPayedPremium: Double,
+    val anpkvPayedPremium: Double = 300.0,
     @ColumnInfo(name = "mitag")  // Zusatzbetrag
-    val mitag: Double,
+    val mitag: Boolean = true,
     @ColumnInfo(name = "nachweis")
-    val nachweis: Boolean,
+    val nachweis: Boolean = false,
     @ColumnInfo(name = "pkpvBasicPremium")
-    val pkpvBasicPremium: Double,
+    val pkpvBasicPremium: Double = 500.0,
 
     /**
      * Optional taxes
      * */
     @ColumnInfo(name = "sonstb")
-    val sonstb: Double,
+    val sonstb: Double = 0.0,
     @ColumnInfo(name = "jsonstb")
-    val jsonstb: Double,
+    val jsonstb: Double = 0.0,
     @ColumnInfo(name = "vmt")
-    val vmt: Double,
+    val vmt: Double = 0.0,
     @ColumnInfo(name = "entsch")
-    val entsch: Double,
+    val entsch: Double = 0.0,
     @ColumnInfo(name = "wfundf")
-    val wfundf: Double,
+    val wfundf: Double = 0.0,
     @ColumnInfo(name = "hinzur")
-    val hinzur: Double,
+    val hinzur: Double = 0.0,
 
     /**
      * Selector children count
      * */
     @ColumnInfo(name = "selectedOptionKinderZahl")
-    val selectedOptionKinderZahl: String,
+    val selectedOptionKinderZahl: String = "--",
 )
