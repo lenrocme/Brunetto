cae8f248c1919f5269ee4fcae18253453f5719a9
lenrocme <mcornelius@mail.ru>
2022-12-29 17:13:33 +0100

    add example colors

--- app/src/main/java/com/example/brunetto/ui/theme/Theme.kt
+++ app/src/main/java/com/example/brunetto/ui/theme/Theme.kt
@@ -1,11 +1,65 @@
 package com.example.brunetto.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
 
+val LightColorPalette = CustomColors(
+    material = lightColors(),
+    main_100 = Light_neumorphism_100,
+    main_200 = Light_neumorphism_200,
+    main_300 = Light_neumorphism_300,
+    main_350 = Light_neumorphism_350,
+    main_400 = Light_neumorphism_400,
+    main_450 = Light_neumorphism_450,
+    main_500 = Light_neumorphism_500,
+    main_600 = Light_neumorphism_600,
+    /**
+     * Font colors
+     * */
+    fontC_100 = fontC_100d,
+    fontC_200 = fontC_200d,
+    fontC_300 = fontC_300d,
+    /**
+     * Main background
+     * */
+    CL_BackGround = COLOR_BackGround,
+    /**
+     * Bottom nav bar
+     * */
+    CL_BottomNavMenuNORMAL = COLOR_BottomNavMenuNORMAL,
+    CL_BottomNavMenuSELECTED = COLOR_BottomNavMenuSELECTED,
+    CL_BottomNavMenu = COLOR_BottomNavMenu,
+)
+
+val DarkColorPalette = CustomColors(
+    material = darkColors(),
+    main_100 = Dark_neumorphism_100,
+    main_200 = Dark_neumorphism_200,
+    main_300 = Dark_neumorphism_300,
+    main_350 = Dark_neumorphism_350,
+    main_400 = Dark_neumorphism_400,
+    main_450 = Dark_neumorphism_450,
+    main_500 = Dark_neumorphism_500,
+    main_600 = Dark_neumorphism_600,
+    /**
+     * Font colors
+     * */
+    fontC_100 = fontC_100l,
+    fontC_200 = fontC_200l,
+    fontC_300 = fontC_300l,
+    /**
+     * Main background
+     * */
+    CL_BackGround = Dark_COLOR_BackGround,
+    /**
+     * Bottom nav bar
+     * */
+    CL_BottomNavMenuNORMAL = Dark_COLOR_BottomNavMenuNORMAL,
+    CL_BottomNavMenuSELECTED = Dark_COLOR_BottomNavMenuSELECTED,
+    CL_BottomNavMenu = Dark_COLOR_BottomNavMenu,
+)
+
+/*
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
@@ -16,17 +70,8 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
-
+)*/
+/*
 @Composable
 fun BrunettoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
@@ -41,4 +86,4 @@ fun BrunettoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
         shapes = Shapes,
         content = content
     )
-}
+}*/
